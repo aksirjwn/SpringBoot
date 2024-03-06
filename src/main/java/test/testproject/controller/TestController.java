dd7585b6cf994d25573d766b73503322e67beb8b
donghee <skks1000@naver.com>
2024-03-06 13:46:42 +0900

    first commit

---
+++ src/main/java/test/testproject/controller/TestController.java
@@ -0,0 +1,14 @@
+package test.testproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+    @GetMapping("testfile") // html 파일명
+    public String controller(Model model) {
+        model.addAttribute("data", "테스트 중"); // html의 값과 들어갈 값의 내용
+        return "testfile"; // html 파일명
+    }
+}
